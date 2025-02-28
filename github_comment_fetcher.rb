dfbb1e76f5aac8748e4c415923e17f6f9dca05dd
watame <pandora.border@gmail.com>
2025-02-28 11:52:17 +0900

    add pr comment print

--- github_comment_fetcher.rb
+++ github_comment_fetcher.rb
@@ -18,22 +18,30 @@ start_time = Time.new(start_date.year, start_date.month, start_date.day, 0, 0, 0
 end_time = Time.new(end_date.year, end_date.month, end_date.day, 23, 59, 59).getlocal('+09:00')
 
 # 自分が作成した PR を取得し、期間内に作成されたものだけフィルタ
-prs = client.pull_requests("#{owner}/#{repo}", state: "all").select do |pr|
+prs = client.pull_requests("#{owner}/#{repo}", per_page: 100, state: "all").select do |pr|
   created_at = pr.created_at.getlocal('+09:00')
   pr.user.login == 'watame' && created_at >= start_time && created_at <= end_time
 end
 
 prs.each do |pr|
   created_at = pr.created_at.getlocal('+09:00')
-  # puts "PR ##{pr.number}: #{pr.title} (#{pr.created_at})"
   puts "PR ##{pr.number}: #{pr.title} (#{created_at})"
 
   # PR のコメントを取得
-  comments = client.pull_request_comments("#{owner}/#{repo}", pr.number)
+  pr_comments = client.pull_request_comments("#{owner}/#{repo}", pr.number)
+  issue_comments = client.issue_comments("#{owner}/#{repo}", pr.number)
 
-  comments.each do |comment|
-    comment_created_at = comment.created_at
-    puts "- #{comment.user.login}: #{comment.body} (#{comment_created_at})"
+  puts "-------PR Comments-------"
+  pr_comments.each do |pr_comment|
+    created_at = pr_comment.created_at.getlocal('+09:00')
+    puts "- #{pr_comment.user.login}: #{pr_comment.body} (#{created_at})"
   end
-  puts "-------------------------"
+  puts "-----Issue Comments------"
+  issue_comments.each do |issue_comment|
+    unless issue_comment.user.login == 'github-actions[bot]'
+      created_at = issue_comment.created_at.getlocal('+09:00')
+      puts "- #{issue_comment.user.login}: #{issue_comment.body} (#{created_at})"
+    end
+  end
+  puts "========================="
 end
